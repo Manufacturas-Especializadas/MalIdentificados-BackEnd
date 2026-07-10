b44ff653609ed7e0da37cda9d8fcd8f0ed60f743
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 10:23:00 -0600

    feat(entitie): entidad cliente

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string ClientName { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<PartNumber> PartNumbers { get; set; } = new List<PartNumber>();
+    }
+}
