0a9f39bd8a2a1ffc9f5ef9a4296930b542b88e32
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 10:24:35 -0600

    feat(entitie): entidad numeros de parte

---
+++ Domain/Entities/PartNumber.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Entities
+{
+    public class PartNumber
+    {
+        public int Id { get; set; }
+
+        public string PartNumbersCode { get; set; } = string.Empty;
+
+        public int IdClient { get; set; }
+
+        public int IdLine { get; set; }
+
+        public int? DefaultStandardPack { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Client? Client { get; set; }
+
+        public Line? Line { get; set; }
+
+        public ICollection<ContainerValidation> ContainerValidations { get; set; } = new List<ContainerValidation>();
+    }
+}
