f1a660f1d6c2e716113f692e14fc49b289ea60c6
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 10:17:49 -0600

    primer commit

---
+++ Domain/Entities/Line.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Line
+    {
+        public int Id { get; set; }
+
+        public string LineName { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<PartNumber> PartNumbers { get; set; } = new List<PartNumber>()
+    }
+}
