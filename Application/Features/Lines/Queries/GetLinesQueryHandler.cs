0ca682f3678c0d35eeaa008accd7c8c1348e7d6c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 11:39:04 -0600

    feat(query): query que trae una lista de lineas

---
+++ Application/Features/Lines/Queries/GetLinesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using static Application.Features.Lines.Queries.GetLines;
+using Microsoft.EntityFrameworkCore;
+using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Features.Lines.Queries
+{
+    public class GetLinesQueryHandler : IRequestHandler<GetLinesQuery, List<LineDto>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetLinesQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<LineDto>> Handle(GetLinesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Lines
+                .Select(line => new LineDto(
+                    line.Id,
+                    line.LineName,
+                    line.IsActive,
+                    line.CreatedAt))
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
