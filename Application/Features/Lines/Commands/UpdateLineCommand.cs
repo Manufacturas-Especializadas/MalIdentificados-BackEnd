a877692d45fe85f0a7d49c4513845d7ef401c43e
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 14:16:27 -0600

    feat(command): command que actualiza una linea

---
+++ Application/Features/Lines/Commands/UpdateLineCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application.Features.Lines.Commands
+{
+    public record UpdateLineCommand(int Id, string LineName, bool IsActive) : IRequest<bool>;
+}
---
+++ Application/Features/Lines/Commands/UpdateLineCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Features.Lines.Commands
+{
+    public class UpdateLineCommandHandler : IRequestHandler<UpdateLineCommand, bool>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateLineCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(UpdateLineCommand request, CancellationToken cancellationToken)
+        {
+            var line = await _context.Lines.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (line == null) return false;
+
+            line.LineName = request.LineName;
+            line.IsActive = request.IsActive;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
