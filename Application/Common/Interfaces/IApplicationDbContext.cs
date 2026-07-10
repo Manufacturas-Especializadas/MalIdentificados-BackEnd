256c671bbe10d6b9fdb0a39ab805ae1ad7e35aaf
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 10:47:36 -0600

    feat(Interface): Interfaz para interactuar con la base de datos desde la capa Application

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Line> Lines { get; }
+        DbSet<Client> Clients { get; }
+        DbSet<PartNumber> PartNumbers { get; }
+        DbSet<ContainerValidation> ContainerValidations { get; }
+        DbSet<ScanDetail> ScanDetails { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
