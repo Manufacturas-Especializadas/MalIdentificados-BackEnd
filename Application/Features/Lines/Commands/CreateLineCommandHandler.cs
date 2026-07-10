0a76a4b95d9e2abc6ca3dc3e42d751eeb8b3f6d0
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 11:24:18 -0600

    feat(command): comando para la creacion de una linea

---
+++ Application/Features/Lines/Commands/CreateLineCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Application.Features.Lines.Commands
+{
+    public class CreateLineCommandHandler
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateLineCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateLineCommand request, CancellationToken cancellationToken)
+        {
+            TimeZoneInfo mexicoTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time (Mexico)");
+
+            DateTime nowInMexico = TimeZoneInfo.ConvertTime(DateTime.UtcNow, mexicoTimeZone);
+
+            var newLine = new Line
+            {
+                LineName = request.LineName,
+                CreatedAt = nowInMexico,
+            };
+
+            _context.Lines.Add(newLine);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return newLine.Id;
+        }
+    }
+}
