6bcad5a8de74f2d920749c27fbc0382a43735018
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 14:44:38 -0600

    feat(dependecy): configuracion de Entity Framework

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("Connection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
