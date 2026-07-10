45fbfbc6c8c96610d9b93a0723980ee6d4b37b5a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 10:35:14 -0600

    feat(entitie): entidad para ver los escaneos por detalles

---
+++ Domain/Entities/ScanDetail.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class ScanDetail
+    {
+        public int Id { get; set; }
+
+        public int IdValidation { get; set; }
+
+        public string ScannedPartCode { get; set; } = string.Empty;
+
+        public bool IsCorrect { get; set; }
+
+        public DateTime ScanDate { get; set; } = DateTime.UtcNow;
+
+        public ContainerValidation? ContainerValidation { get; set; }
+    }
+}
