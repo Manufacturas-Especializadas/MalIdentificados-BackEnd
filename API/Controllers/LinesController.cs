9343c745af11ff9bf052b736385080ba10655138
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 11:47:08 -0600

    feat(controller, csproj): controlador con endpoints GET Y POST

---
+++ API/Controllers/LinesController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.Lines.Commands;
+using Application.Features.Lines.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LinesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LineDto>>> GetLines()
+        {
+            var result = await _mediator.Send(new GetLinesQuery());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateLine([FromBody] CreateLineCommand command)
+        {
+            var lineId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetLines), new { id = lineId }, lineId);
+        }
+    }
+}
