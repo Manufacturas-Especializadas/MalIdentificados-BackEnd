9165dd34098045c535e75422fee9849edb1c347b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 11:27:14 -0600

    feat(Query): Dto y query que obtienen una lista de lineas de produccion

---
+++ Application/Features/Lines/Queries/GetLines.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Features.Lines.Queries
+{
+    public class GetLines
+    {
+        public record LineDto(int Id, string LineName, bool IsActive, DateTime CreatedAt);
+
+        public record GetLinesQuery() : IRequest<List<LineDto>>;
+    }
+}
