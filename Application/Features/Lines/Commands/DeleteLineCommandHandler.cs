b59ba50c12605ecb17ead32fd81d48e7e7c374a8
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-10 14:19:05 -0600

    feat(command): command que desactiva una linea

---
+++ Application/Features/Lines/Commands/DeleteLineCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Features.Lines.Commands
+{
+    public class DeleteLineCommandHandler : IRequestHandler<DeleteLineCommand, bool>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteLineCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteLineCommand request, CancellationToken cancellationToken)
+        {
+            var line = await _context.Lines.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (line == null) return false;
+
+            line.IsActive = false;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
