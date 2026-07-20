b3159a6db104a41e3e2c31078b2e8e75abfc096f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 14:43:19 -0600

    feat(handler): metodos para la verificacion

---
+++ Application/Features/Scanning/Query/ValidateQualityApproverQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application.Features.Scanning.Query
+{
+    public record ValidateQualityApproverQuery(int PayrollNumber) : IRequest<bool>;
+}
---
+++ Application/Features/Scanning/Query/ValidateQualityApproverQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Scanning.Query;
+using Microsoft.EntityFrameworkCore;
+using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Features.Quality.Queries.ValidateQualityApprover
+{
+    public class ValidateQualityApproverQueryHandler : IRequestHandler<ValidateQualityApproverQuery, bool>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ValidateQualityApproverQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(ValidateQualityApproverQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.QualityApprovers
+                .AnyAsync(q => q.PayrollNumber == request.PayrollNumber && q.IsActive, cancellationToken);
+        }
+    }
+}
