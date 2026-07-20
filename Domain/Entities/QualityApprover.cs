e8bfc5464266352a31a0b6fc1bb43eb38d3c4832
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-20 14:39:53 -0600

    feat(entities): nuevas entidades

---
+++ Domain/Entities/QualityApprover.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class QualityApprover
+    {
+        public int Id { get; set; }
+
+        public int PayrollNumber { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<ScanDetail> ScanDetails { get; set; } = new List<ScanDetail>();
+    }
+}
